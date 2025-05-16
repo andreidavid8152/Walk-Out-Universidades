f10c4d718e546fe98799d95dc81d7358f898de11
(Estudiante) Andrei David Flores Espinoza <andrei.flores@udla.edu.ec>
2025-05-16 15:21:07 -0500

    Mapa calor de universidades

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.main import main_bp
+from routes.mapa_calor_universidades import mapa_uni_bp
 
 app = Flask(__name__)
 app.register_blueprint(main_bp)
+app.register_blueprint(mapa_uni_bp)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
